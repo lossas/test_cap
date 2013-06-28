5767f09c2ba6c51c14e6926cdcdad972b6122cda
Leonidas <lossas85@gmail.com>
2013-06-28 13:49:51 +0200

    initial commit

---
+++ config/deploy/default.rb
@@ -0,0 +1,2 @@
+server "192.168.42.152", :app, :web, :db, :primary => true
+set :deploy_to, "/home/leonidas/www/test_cap/default_stage/
---
+++ config/deploy/sec_stage.rb
@@ -0,0 +1,2 @@
+server "192.168.42.52", :app, :web, :db, :primary => true
+set :deploy_to, "/home/leonidas/www/test_cap/sec_stage
